1eb6f68eabc9c28c1ea7be50f50039fc60425de8
Guillaume Coupelant <guillaume.coupelant@gmail.com>
2014-09-13 23:57:44 +0200

    Update angular-file-upload.js

--- dist/angular-file-upload.js
+++ dist/angular-file-upload.js
@@ -205,7 +205,6 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 		if ('draggable' in document.createElement('span')) {
 			var leaveTimeout = null;
 			elem[0].addEventListener("dragover", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 				$timeout.cancel(leaveTimeout);
 				if (!elem[0].__drag_over_class_) {
@@ -222,7 +221,6 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 				elem.addClass(elem[0].__drag_over_class_);
 			}, false);
 			elem[0].addEventListener("dragenter", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 			}, false);
 			elem[0].addEventListener("dragleave", function(evt) {
@@ -233,7 +231,6 @@ angularFileUpload.directive('ngFileDrop', [ '$parse', '$timeout', '$location', f
 			}, false);
 			var fn = $parse(attr['ngFileDrop']);
 			elem[0].addEventListener("drop", function(evt) {
-				evt.stopPropagation();
 				evt.preventDefault();
 				elem.removeClass(elem[0].__drag_over_class_);
 				elem[0].__drag_over_class_ = null;
