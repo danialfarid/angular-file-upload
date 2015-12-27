b1ab3e3fbae566d5f39370e0a9bd6fc88f2ef87a
alexanderman <alexander.shnirman@gmail.com>
2015-12-27 14:39:41 +0200

    fixed ngFileUploadApeConfig

--- src/ngFileUploadApeConfig.js
+++ src/ngFileUploadApeConfig.js
@@ -2,13 +2,8 @@
 
   ngFileUpload.provider('ngFileUploadApeConfig', function(){
 
-    var _skipResizingGif = true;
     var _imageProxyUrl = 'http://url.apester.com/';
 
-    this.skipGifResizing = function(isSkip){
-      _skipResizingGif = isSkip;
-      return this;
-    };
     this.imageProxyUrl = function(proxyUrl){
       _imageProxyUrl = proxyUrl;
       return this;
@@ -16,7 +11,6 @@
 
     this.$get = [function(){
       return {
-        skipGifResizing:  _skipResizingGif,
         imageProxyUrl:    _imageProxyUrl
       };
     }];
