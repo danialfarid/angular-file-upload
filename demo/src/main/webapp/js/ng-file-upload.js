fbcf141abcfb75e97e44315cd13184d1f2449ff8
ray-peters <me@raypeters.com>
2016-03-04 12:40:31 -0800

    Fixes issue #1388 by allowing maxFile errors to skip appending files to the invalids array.

--- demo/src/main/webapp/js/ng-file-upload.js
+++ demo/src/main/webapp/js/ng-file-upload.js
@@ -577,10 +577,14 @@ ngFileUpload.service('Upload', ['$parse', '$timeout', '$compile', '$q', 'UploadE
     }
 
     function separateInvalids() {
+      var skipErrors = [ 'maxFiles' ];
+
       valids = [];
       invalids = [];
+
       angular.forEach(allNewFiles, function (file) {
         if (file.$error) {
+          if ( skipErrors.indexOf( file.$error ) !== -1 ) return;
           invalids.push(file);
         } else {
           valids.push(file);
