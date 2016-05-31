e89a2db6e72e350da20c2d86c08b31a783183965
Leonardo Bazico <leonardobazico@gmail.com>
2016-05-31 17:22:49 -0300

    dismissing null value, string null and string undefined

--- src/upload.js
+++ src/upload.js
@@ -251,7 +251,7 @@ ngFileUpload.service('UploadBase', ['$http', '$q', '$timeout', function ($http, 
     }
 
     function addFieldToFormData(formData, val, key) {
-      if (val !== undefined) {
+      if (val !== undefined && val !== 'undefined' && val !== null && val !== 'null') {
         if (angular.isDate(val)) {
           val = val.toISOString();
         }
